f6f55c5ba00322948d0df49817a8ad5bd2fc5f5a
James Russo <1jlrusso@gmail.com>
2018-04-02 19:04:43 -0700

    this file contains the nav an search sections that will be linked to content pages using php 'include_once' statement

---
+++ navSearch.php
@@ -0,0 +1,97 @@
+<?php
+echo "
+  <nav class='clearfix'>
+    <div class='nav-logo animated fadeInRight'><a href='phyCalc.php'>PhyCalc</a></div>
+    <div class='navigation animated fadeInLeft'>
+      <ul class='horizontal-nav'>
+        <li><a href='#' id='contact-btn'>Contact</a></li>
+        <li><a href='#' id='calculator-btn'>Calculator</a></li>
+        <li><a href='#' id='conversion-btn'>Conversions</a></li>
+        <li><a href='#' id='horizontal-search-btn'><i class='glyphicon glyphicon-search'></i></a></li>
+      </ul>
+      <div class='animated fadeInLeft' id='icon'>
+        <div id='vertical-search-btn'><i class='glyphicon glyphicon-search'></i></div>
+        <div id='click-box'></div>
+        <span id='hamburger-menu'></span>
+        <div id='vertical-nav' class='vertical-nav'>
+          <ul>
+            <li><a href='#' id='vert-contact-btn'>Contact</a></li>
+            <li><a href='#' id='vert-calculator-btn'>Calculator</a></li>
+            <li><a href='#' id='vert-conversion-btn'>Conversions</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <div id='search-section'>
+    <div id='search-form'>
+      <input type='text' id='search-bar' placeholder='Search'/>
+      <div id='search-icon'><i class='glyphicon glyphicon-search'></i></div>
+    </div>
+    <div id='close-search-btn'>
+      <span id='close-bar-one'></span>
+      <span id='close-bar-two'></span>
+    </div>
+    <div id='search-case-list'>
+      <a href='displacement.html'>displacement</a>
+      <a href='displacement.html'>initial position</a>
+      <a href='displacement.html'>final position</a>
+      <a href='displacement.html'>position motion</a>
+      <a href='displacement.html'>negative motion</a>
+      <a href='displacement.html'>distance and displacement</a>
+      <a href='displacement.html'>position and time</a>
+      <a href='displacement.html'>net displacement</a>
+      <a href='vectorDisplacement.html'>vectors</a>
+      <a href='vectorDisplacement.html'>vector displacement</a>
+      <a href='vectorDisplacement.html'>multi-dimensional displacement</a>
+      <a href='vectorDisplacement.html'>vector identity</a>
+      <a href='vectorDisplacement.html'>angled displacement</a>
+      <a href='vectorDisplacement.html'>angled travel</a>
+      <a href='vectorDisplacement.html'>displacement trigonometry</a>
+      <a href='vectorDisplacement.html'>inverse trigonometry</a>
+      <a href='vectorDisplacement.html'>pythagorean theorem</a>
+      <a href='vectorDisplacement.html'>vector addition</a>
+      <a href='velocity.html'>solving velocity</a>
+      <a href='velocity.html'>velocity and displacement</a>
+      <a href='velocity.html'>velocity and time interval</a>
+      <a href='velocity.html'>displacement and time interval</a>
+      <a href='velocity.html'>speed and velocity</a>
+      <a href='acceleration.html'>acceleration</a>
+      <a href='acceleration.html'>changing velocity</a>
+      <a href='acceleration.html'>time and acceleration</a>
+      <a href='acceleration.html'>negative acceleration</a>
+      <a href='acceleration.html'>positive acceleration</a>
+      <a href='acceleration.html'>velocity and acceleration</a>
+      <a href='acceleration.html'>time and acceleration</a>
+      <a href='kinematics.html'>kinematics</a>
+      <a href='kinematics.html'>big five</a>
+      <a href='kinematics.html'>initial velocity kinematics</a>
+      <a href='kinematics.html'>final velocity kinematics</a>
+      <a href='kinematics.html'>initial position kinematics</a>
+      <a href='kinematics.html'>final position kinematics</a>
+      <a href='kinematics.html'>acceleration kinematics</a>
+      <a href='kinematics.html'>constant acceleration</a>
+      <a href='kinematics.html'>constant velocity</a>
+      <a href='kinematics.html'>velocity and acceleration kinematics</a>
+      <a href='circularMotion.html'>angular velocity</a>
+      <a href='circularMotion.html'>period circular motion</a>
+      <a href='circularMotion.html'>radius circular motion</a>
+      <a href='circularMotion.html'>uniform circular motion</a>
+      <a href='circularMotion.html'>frequency circular motion</a>
+      <a href='forces.html'>solving net force</a>
+      <a href='forces.html'>one dimensional forces</a>
+      <a href='forces.html'>mass and acceleration</a>
+      <a href='forces.html'>newton's laws</a>
+      <a href='forces.html'>normal forces</a>
+      <a href='gravitationalForces.html'>gravitational forces</a>
+      <a href='gravitationalForces.html'>newton's gravitational laws</a>
+      <a href='gravitationalForces.html'>inverse square law</a>
+      <a href='gravitationalForces.html'>action reaction pairs</a>
+      <a href='gravitationalForces.html'>gravity and acceleration</a>
+      <a href='gravitationalForces.html'>gravitational constant</a>
+    </div>
+  </div>
+
+";
+?>
