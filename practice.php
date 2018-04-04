b115580d4aebc15c744694f0f03ee7ff1838ff21
James Russo <1jlrusso@gmail.com>
2018-04-04 16:15:01 -0700

    this file contains 'practice-btns-outer' section that is linked to velocity.php using php 'include' statement

---
+++ practice.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "
+  <div class='row text-center' id='practice-btns-outer'>
+    <div class='text-center' id='practice-btns-container'>
+      <button type='button' class='practice-btn' data-toggle='modal' data-target='#my-modal-one'>Example 1</button>
+      <button type='button' class='practice-btn' data-toggle='modal' data-target='#my-modal-two'>Example 2</button>
+      <button type='button' class='practice-btn' data-toggle='modal' data-target='#my-modal-three'>Example 3</button>
+    </div>
+  </div>
+";
